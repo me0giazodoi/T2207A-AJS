f02cc5aaee82044dda1c1564544842860b06a91c
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2022-10-05 14:50:20 +0700

    btap ve nha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,22 @@ import {StudentComponent} from "./student/student.component";
 import {TeacherComponent} from "./teacher/teacher.component";
 import {LoginComponent} from "./Btapbuoi4/Login.component";
 import {RegisterComponent} from "./Btapbuoi4/Register.component";
+import {RouterModule, Routes} from "@angular/router";
+import {WeatherComponent} from "./Btapbuoi4/Weather.component";
 
+const appRoutes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'Register',component:RegisterComponent},
+  {path:'Weather',component:WeatherComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent, ClassroomComponent, StudentComponent, TeacherComponent, LoginComponent, RegisterComponent
+    AppComponent,
+    ClassroomComponent, StudentComponent, TeacherComponent,
+    LoginComponent, RegisterComponent, WeatherComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
